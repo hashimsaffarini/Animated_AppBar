d456954523c501d1305cddc079aca693f727eac1
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-09-12 16:07:31 +0300

    Refactor AnimatedAppBar to use animation controller and transitions

--- lib/widgets/animated_app_bar.dart
+++ lib/widgets/animated_app_bar.dart
@@ -2,45 +2,102 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AnimatedAppBar extends StatelessWidget {
+class AnimatedAppBar extends StatefulWidget {
   const AnimatedAppBar({super.key});
 
   @override
+  State<AnimatedAppBar> createState() => _AnimatedAppBarState();
+}
+
+class _AnimatedAppBarState extends State<AnimatedAppBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+  late Animation<Offset> _animation2;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _animation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _animation2 = Tween<Offset>(
+      begin: const Offset(1, 0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea( 
+    return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Row(
           children: [
-            Image.asset(
-              'assets/images/memoji.png',
-              height: 60,
+            SlideTransition(
+              position: _animation,
+              child: Image.asset(
+                'assets/images/memoji.png',
+                height: 60,
+              ),
             ),
             const SizedBox(width: 6),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Good Morning 👋',
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+            SlideTransition(
+              position: _animation2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Good Morning 👋',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  'Hashim Saffarini',
-                  style: GoogleFonts.poppins(
-                    color: const Color(0xffA8A8A7),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+                  Text(
+                    'Hashim Saffarini',
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xffA8A8A7),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             const Spacer(),
-            SvgPicture.asset(
-              'assets/svgs/Shape.svg',
+            SlideTransition(
+              position: _animation2,
+              child: SvgPicture.asset(
+                'assets/svgs/Shape.svg',
+              ),
             ),
           ],
         ),
